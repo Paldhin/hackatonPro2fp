cad594d50ea8808248a6300d10f58ad93a26567b
Diego <diego.sancon12@gmail.com>
2024-06-06 22:06:38 +0200

    empezando el proyecto

---
+++ src/es/pro2fp/programacion/clases/Direccion.java
@@ -0,0 +1,17 @@
+package es.pro2fp.programacion.clases;
+
+public class Direccion {
+
+    private String pais;
+    private String provincia;
+    private String ciudad;
+    private String calle;
+    private String numero;
+    private String piso;
+    private String puerta;
+
+    public Direccion (String pais, String provincia, String ciudad, String calle, String numero, String piso, String puerta){
+        this.pais = pais;
+        this.provincia = provincia;
+    }
+}
---
+++ src/es/pro2fp/programacion/clases/Usuario.java
@@ -0,0 +1,9 @@
+package es.pro2fp.programacion.clases;
+
+public class Usuario {
+
+    private String nombre;
+    private String apelldos;
+    private String dni;
+    private String edad;
+}
