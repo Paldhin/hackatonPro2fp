0286757b660be862bdf71d9d85ad75db2a92013c
vcerdan <v.cerdan@pro2fp.es>
2024-06-07 02:26:30 +0200

    Empezando insercion de hotel

---
+++ src/es/pro2fp/programacion/Utiles/InsercionABaseDatos.java
@@ -0,0 +1,21 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class InsercionABaseDatos {
+  
+    
+
+public static void insertHotel(Hotel hotel){
+    ResultSet rs;
+    PreparedStatement ps;
+
+    rs.insertRow();
+    ps = 
+
+
+}
+
+
+
+
+}
