a4c2695fa891bb73eb811aa68b1a966c366bec85
Diego <diego.sancon12@gmail.com>
2024-06-07 04:28:47 +0200

    Clase Usuario

--- src/es/pro2fp/programacion/main/mainDiego.java
+++ src/es/pro2fp/programacion/main/mainDiego.java
@@ -1,4 +1,5 @@
 package es.pro2fp.programacion.main;
+import es.pro2fp.programacion.Excepciones.ExcepcionDni;
 import es.pro2fp.programacion.Excepciones.ExcepcionTelefono;
 import es.pro2fp.programacion.Utiles.Conexion;
 import es.pro2fp.programacion.clases.Direccion;
@@ -8,7 +9,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class main {
+public class mainDiego {
     static Conexion con;
     public static void main(String[] args) {
         con = new Conexion();
@@ -17,31 +18,53 @@ public class main {
         char gestionMenu = 0;
         Scanner sc = new Scanner(System.in);
         try {
-            //Usuario usuario = CrearUsuario();
-            // con.insertarUsuario(usuario);
-            Usuario usuario = IniciarSesion();
+            Usuario usuario = CrearUsuario();
+            con.insertarUsuario(usuario);
+            usuario = IniciarSesion();
             System.out.println(usuario.getDni());
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
-        
-        // //Primera parte del programa: login y register
-        // switch (gestionMenu) {
-        //     case '1':
-        // }
 
-        // do {
-        //     gestionMenu = sc.next().charAt(0);
-        //     switch (gestionMenu) {
-        //         case '1':
-        //     }
-        // } while (gestionMenu != '0');
+
+        //Primera parte del programa: login y register
+        do {
+            menuUsuario1();
+            gestionMenu = sc.next().charAt(0);
+            switch (gestionMenu) {
+                case '1':
+                    try {
+                        Usuario usuario = CrearUsuario();
+                        con.insertarUsuario(usuario);
+                    } catch (ExcepcionTelefono | ExcepcionDni e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case '2':
+                    Usuario usuario = IniciarSesion();
+                    break;
+                case '0':
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("El valor introducido no es válido. Introduzca un valor entre '1', '2', y '0'.");
+                    break;
+            }
+        } while (gestionMenu != '1' && gestionMenu != '2' && gestionMenu != '0');
+
+        do {
+            gestionMenu = sc.next().charAt(0);
+            switch (gestionMenu) {
+                case '1':
+            }
+        } while (gestionMenu != '0');
+
 
 
     }
     public static void menuUsuario1() {
         System.out.println("""
+                \nBIENVENIDO A HOTEL TRIVAGO
                 ----------------------
                 1.- Registrarse
                 2.- Inicio de Sesión
@@ -85,46 +108,63 @@ public class main {
         return usuario;
     }
 
-    public static Usuario CrearUsuario() throws ExcepcionTelefono {
+    public static Usuario CrearUsuario() throws ExcepcionTelefono, ExcepcionDni {
         System.out.println("Introduce tu nombre:");
         String nombre = GetString();
+
         System.out.println("Introduce tu nombre usuario:");
         String nombre_usuario = GetString();
+
         System.out.println("Introduce tu primer apellido:");
         String apellido1 = GetString();
+
         System.out.println("Introduce tu segundo apellido:");
         String apellido2 = GetString();
+
         System.out.println("Introduce un numero de teléfono:");
         String telefono = GetTelefono();
+
         System.out.println("Introduce un correo electrónico:");
         String email = GetString();
+
         System.out.println("Introduce tu DNI:");
-        String dni = GetString();
+        String dni = GetDNI();
+
         System.out.println("¿Eres administrador? (S/N)");
         boolean administrador = false;
         if (GetString().equalsIgnoreCase("S")) administrador = true;
+
         System.out.println("Introduce una contraseña:");
         String password = GetString();
+
         // Creamos los campos necesarios para la direccion
         System.out.println("Introduce tu calle:");
         String calle = GetString();
+
         System.out.println("Introduce tu número:");
         String numero = GetString();
+
         System.out.println("Introduce tu puerta:");
         String puerta = GetString();
+
+        System.out.println("Introduce tu provincia:");
+        String provincia = GetString();
+
         System.out.println("Introduce tu ciudad:");
         String ciudad = GetString();
+
         System.out.println("Introduce tu municipio:");
         String municipio = GetString();
 
         System.out.println("Introduce tu código postal:");
         String codigoPostal = GetString();
+
         System.out.println("Introduce tu país:");
         String pais = GetString();
 
         Direccion direccion = new Direccion(-1, calle,numero,puerta,ciudad,municipio,codigoPostal,pais);
         Usuario usuarioTMP = new Usuario(nombre_usuario,nombre, apellido1, apellido2, telefono,email,dni,direccion,administrador,-1,password);
-        
+
         return usuarioTMP;
     }
 
@@ -142,12 +182,29 @@ public class main {
 
     public static String GetTelefono () throws ExcepcionTelefono {
         String tlf = GetString();
-        boolean bool = comprobacionTelefono(tlf);
+        boolean bool = ComprobacionTelefono(tlf);
         if (!bool) System.out.println("El teléfono introducido no existe.");
         return tlf;
     }
 
-    public static boolean comprobacionTelefono (String telefono) throws ExcepcionTelefono {
+    public static String GetDNI () throws ExcepcionDni {
+        String dni = GetString();
+        boolean bool = ComprobacionDNI(dni);
+        if (!bool) System.out.println("El DNI introducido no existe o no es correcto.");
+        return dni;
+    }
+
+    public static boolean ComprobacionDNI(String dni) throws ExcepcionDni {
+        Pattern p = Pattern.compile("[0-9]{8}[a-zA-Z]{1}");
+        Matcher m = p.matcher(dni);
+        if (!m.matches()) {
+            throw new ExcepcionDni("El DNI no sigue el parámetro correcto");
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean ComprobacionTelefono(String telefono) throws ExcepcionTelefono {
         Pattern p = Pattern.compile("[6-7]{1}[0-9]{8}");
         Matcher m = p.matcher(telefono);
         System.out.println(telefono+"----");
